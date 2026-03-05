6123ff605b1114f1e42e3592e5094b4c93587b18
Anandh221610 <anandh.sakthivel@boltin.com>
2026-03-05 11:40:59 +0000

    feat(ui): configure Material Design 3 theme with React Native Paper

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,18 @@
 import { Stack } from "expo-router";
+import { useColorScheme } from 'react-native';
+import {
+  MD3DarkTheme,
+  MD3LightTheme,
+  PaperProvider
+} from 'react-native-paper';
+import { Colors } from '../constants/colors';
 
 export default function RootLayout() {
-  return <Stack />;
+  const colorScheme = useColorScheme();
+  const theme = colorScheme === 'dark' ? { ...MD3DarkTheme, colors: Colors.dark } : { ...MD3LightTheme, colors: Colors.light }
+  return (
+    <PaperProvider theme={theme}>
+      <Stack />
+    </PaperProvider>
+  )
 }
